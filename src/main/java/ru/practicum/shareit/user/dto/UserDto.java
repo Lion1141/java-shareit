0e789e2ae9a07b6c02aa880b6dfe0c7921c74159
Лев <vodopyanov.le@yandex.ru>
2024-04-02 22:15:49 +1000

    feat: Выполнено ФЗ №13, реализованы базовые классы моделей, сервисов и контроллеров для ShareIt

---
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    @NotNull
+    @NotBlank
+    private String name;
+    @Email
+    @NotNull
+    @NotBlank
+    private String email;
+}
